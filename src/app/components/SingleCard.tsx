24d296c4eae8ab4d2463b04ad70ca7f019119430
Arnold Thabo Sethaba <atsethaba@gmail.com>
2023-11-22 16:16:57 +0200

    Made a skeleton for the front end

---
+++ src/app/components/SingleCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "../styles.module.css";
+
+interface SingleCardProps {
+  card: {
+    f_src: string;
+    b_src: string;
+  };
+  handleChoice(card: any): any;
+  flipped: boolean;
+  disabled: boolean;
+}
+
+function SingleCard({
+  card,
+  handleChoice,
+  flipped,
+  disabled,
+}: SingleCardProps) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card flex justify-center">
+      <div className={flipped ? `${styles.flipped}` : ""}>
+        <img
+          src={card.f_src}
+          className={`${styles.front} rounded-xl shadow-lg`}
+          alt="card front"
+        />
+
+        <img
+          src={card.b_src}
+          className={`${styles.back} rounded-xl shadow-lg`}
+          alt="card back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SingleCard;
